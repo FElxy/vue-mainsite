f423f1b66c5737e984b2b0c3d4a08151375b3c77
mansu <mansu.lxy@gmail.com>
2017-05-23 18:43:51 +0800

    build

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,35 @@
 <template>
 	<div>
-		<div class="nav">
-			<a href="https://FElxy.github.io/blog">blog</a>
-		</div>
-		<div>constructing</div>	
+		<ul class="list">
+			<li class="item">---</li>
+			<li class="item">
+				<a href="https://FElxy.github.io/blog">blog</a>
+			</li>
+			<li class="item">---</li>
+		</ul>	
 	</div>
 
 </template>
+
+<style lang="less">
+	.list {
+		display: flex;
+		align-item: middle;
+		justify-content: space-between;
+		width: 500px;
+		margin: 0 auto;
+		.item {
+			width: 150px;
+			height: 150px;
+			line-height: 150px;
+			border-radius: 50%;
+			background: #333;
+			color: #fff;
+			font-size: 32px;
+			a {
+				display: block;
+				color: #fff;
+			}
+		}
+	}
+</style>
